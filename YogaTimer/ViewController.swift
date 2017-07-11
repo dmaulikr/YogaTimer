b64311f2dbec52bfd148fa1f9195bb2ddead2c57
Sushrut Shringarputale <sush.shring@gmail.com>
2017-07-11 12:07:12 +0530

    First Commit

--- YogaTimer/ViewController.swift
+++ YogaTimer/ViewController.swift
@@ -5,13 +5,18 @@
 //  Created by Sushrut Shringarputale on 7/10/17.
 //  Copyright © 2017 Sushrut Shringarputale. All rights reserved.
 //
-
 import UIKit
+import MaterialComponents.MaterialButtons
+import MBCircularProgressBar
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        let yogaFiller : MBCircularProgressBarView = self.view.viewWithTag(<#T##tag: Int##Int#>)
+        self.view.addSubview(yogaFiller)
+        let raisedButton = MDCRaisedButton.init()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
